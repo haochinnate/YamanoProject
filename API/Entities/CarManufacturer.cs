1ce71a4493a3d93dd68863b61555ce2bd5c76aa6
Qineria <haochinnate@gmail.com>
2020-12-01 13:06:35 +0800

    first time update YamanoProject

---
+++ API/Entities/CarManufacturer.cs
@@ -0,0 +1,10 @@
+namespace API.Entities
+{
+    public class CarManufacturer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ChineseName { get; set; }
+        public string LogoUrl { get; set; }
+    }
+}
