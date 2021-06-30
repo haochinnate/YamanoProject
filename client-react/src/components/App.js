122cec1d4fa630c845ef8228b08fb17183bc0407
Qineria <haochinnate@gmail.com>
2021-06-30 18:00:17 +0800

    install react-router-dom

---
+++ client-react/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <div className="ui container">
+            App
+        </div>
+    );
+};
+
+export default App;
