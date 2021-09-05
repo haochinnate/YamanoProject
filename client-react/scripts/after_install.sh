bc27ba64dde48a4d173abc62499f223c742b0af7
Qineria <haochinnate@gmail.com>
2021-09-05 23:37:24 +0800

    use another tutorial to deploy

--- client-react/scripts/after_install.sh
+++ client-react/scripts/after_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/server
+cd /home/ubuntu/fortune
 npm install
 npm install --save react react-dom react-scripts react-particles-js
 npm install pm2 -g
--- client-react/scripts/app_start.sh
+++ client-react/scripts/app_start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-cd /home/ubuntu/server/src
-sudo npm start
+sudo chmod -R 777 /home/ubuntu/fortune
+cd /home/ubuntu/fortune/build
+pm2 start /home/ubuntu/fortune/node_modules/react-scripts/scripts/start.js --name “fortune”
+#sudo npm start
 #pm2 start npm --name "yamano" -- start
 #pm2 startup
 #pm2 save
