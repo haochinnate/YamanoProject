41a9bdedd36c3358565edcc8b60150e58219565e
Qineria <haochinnate@gmail.com>
2021-09-06 23:34:04 +0800

    do not install

--- client-react/scripts/before_install.sh
+++ client-react/scripts/before_install.sh
@@ -2,5 +2,5 @@
 cd /home/ubuntu/fortune
 #curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 #sudo apt install libssl1.1=1.1.1f-1ubuntu2
-sudo apt install npm
+# sudo apt install npm
 #sudo apt install -y nodejs
